af79999fd81df0dff94bbebef2fc33aca999a96d
Lucas Pereira <142121486+lucasrp134@users.noreply.github.com>
2026-06-22 00:51:28 -0300

    Update Home.tsx
    Codigo original copiado e adaptado para React, e css importado de arquivo separado, e @ts-ignore usado para evitar erro de importação

---
+++ React/src/Pages/Home.tsx
@@ -0,0 +1,97 @@
+import { Link } from "react-router-dom";
+// @ts-ignore
+import './HomeStyle.css';
+// @ts-ignore
+import './HeaderStyle.css';
+// @ts-ignore
+import './FooterStyle.css';
+
+export default function Home() {
+    return (
+        <>
+            <header> 
+                <section>
+                    <figure>
+                        <img src="../Images/logoPet.png" id="Logo" alt="Logo Pet" />
+                    </figure>           
+                </section>
+                <section className="botoes_topo">
+                    <Link to="/Produtos">
+                        <button className="botao_topo">Produtos</button>
+                    </Link>
+                    <Link to="/Servicos">
+                        <button className="botao_topo">Serviços</button>
+                    </Link>
+                    <Link to="/SobreNos">
+                        <button className="botao_topo">Sobre nós</button>
+                    </Link>
+                    <Link to="/OndeEstamos">
+                        <button className="botao_topo">Onde estamos</button>
+                    </Link>
+                    <Link to="/Login">
+                        <button className="botao_topo">Login</button>
+                    </Link>
+                </section>
+                <section className="icones">
+                    <span className="icon material-symbols-outlined">search</span>
+                </section>
+                <section className="icone_cart">
+                    <button className="botao_cart">
+                         <Link to="/Carrinho">
+                            <img src="../Images/CarrinhoCompras.png" id="cart" alt="Carrinho" />
+                         </Link>
+                    </button>
+                </section>
+            </header>
+
+            <main> 
+                <section>
+                    <figure className="figura_frente">
+                        <img src="../Images/frente_petshop.png" id="frente_petshop" alt="Frente Petshop" />
+                    </figure>
+                </section>    
+            </main>
+            
+            <footer>
+                <div className="footer_cima">
+                    <section className="logos_baixo">
+                        <button className="logo_baixo">
+                            <figure>
+                                <img src="../Images/whatsapp.png" id="Logo_baixo" alt="Whatsapp" />
+                            </figure>
+                        </button>
+                        <button className="logo_baixo">
+                            <figure>
+                                <img src="../Images/facebook.png" id="Logo_baixo" alt="Facebook" />
+                            </figure>
+                        </button>
+                        <button className="logo_baixo">
+                            <figure>
+                                <img src="../Images/instagram.png" id="Logo_baixo" alt="Instagram" />
+                            </figure>
+                        </button>
+                    </section>
+                    <section className="botoes_baixo">
+                        <Link to="/">
+                            <button className="botao_baixo">Home</button>
+                        </Link>
+                        <Link to="/SobreNos">
+                            <button className="botao_baixo">Sobre nós</button>
+                        </Link>
+                        <Link to="/OndeEstamos">
+                            <button className="botao_baixo">Nos contate</button>
+                        </Link>
+                    </section>
+                </div>
+                
+                <div>
+                    <section className="footer_baixo">
+                        <p className="footer_texto">
+                            Copyright © 2024 EJPET. Todos os direitos reservados.
+                        </p>
+                    </section>
+                </div>
+            </footer>
+        </>
+    );
+}
